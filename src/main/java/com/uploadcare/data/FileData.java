1eca55115afe6786568254996562f13b7e392ce7
Yervand Aghababyan <yervand.aghababyan@sflpro.com>
2014-12-22 01:09:39 +0400

    Removed uneeded commented-out lines from the previous version of the API

--- src/main/java/com/uploadcare/data/FileData.java
+++ src/main/java/com/uploadcare/data/FileData.java
@@ -4,13 +4,7 @@ import java.net.URI;
 import java.util.Date;
 
 public class FileData {
-//    public String fileId; // version 0.2
-//    public Date lastKeepClaim; // version 0.2
-//    public boolean madePublic; // version 0.2
-//    public boolean onS3; // version 0.2
-//    public Date removed; // version 0.2
-//    public Date uploadDate; // version 0.2
-    
+
     public URI originalFileUrl;
     public String mimeType;
     public boolean isReady;
